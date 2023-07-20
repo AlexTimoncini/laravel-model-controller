aae41f920e677cfadfc3f3a2881bd6dfed8c52b2
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-07-20 16:28:22 +0200

    fixed link with db and displayed movies in bootstrap cards

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,6 +3,22 @@
 @section('title', 'Home')
 
 @section('main-section')
-    <h1>Hello There, You're such a cute human being!</h1>
-    @dd($books);
+
+<div class="container">
+    <div class="row">
+        @foreach ($movies as $movie)
+        <div class="card col-3 m-3">
+            <div class="card-body d-flex flex-column">
+                <h4 class="card-title text-primary">{{ $movie->title }}</h4>
+                <h6 class="card-title text-secondary">{{ $movie->original_title }}</h6>
+                <div class="ivy_down_wrap mt-auto">
+                    <p class="card-text text-capitalize">{{ $movie->nationality }} -- {{ $movie->date }}</p>
+                    <a href="#" class="btn btn-primary">Enjoy it now!</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+
 @endsection
